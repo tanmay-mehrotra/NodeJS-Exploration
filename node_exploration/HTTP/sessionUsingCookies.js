2585c3b5672aee292146ecf5e1f735a9c66de091
Tanmay Mehrotra <tanmaymehrotra86@gmail.com>
2014-12-02 14:17:18 +0400

    socket.io example added

---
+++ node_exploration/HTTP/sessionUsingCookies.js
@@ -0,0 +1,24 @@
+var connect = require('connect');
+var format = require('util').format;
+
+var app = connect();
+
+app.use(connect.query());
+
+app.use(connect.cookieParser('this is my secret string'));
+
+app.use(connect.session({
+	cookie :{
+		maxAge : 24 * 60 * 60 * 1000
+	}
+}));
+
+//response
+app.use(function(req, res){
+	for(var name in req.query){
+		req.session[name] = req.query[name];
+	}
+	res.end(format(req.session) + '\n');
+});
+
+app.listen(8080);
